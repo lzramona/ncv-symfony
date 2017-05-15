ea20411232813cbebdf2fddc73ef974998480001
Ramona Lazar <ramona@a2.nl>
2017-05-15 14:01:36 +0300

    Upload project 15.05.2017

---
+++ src/AppBundle/Menu/Builder.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ramona
+ * Date: 28.03.2017
+ * Time: 22:57
+ */
+
+namespace AppBundle\Menu;
+
+
+class Builder
+{
+
+}
---
+++ src/CarBundle/Entity/Ncvuser.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ramona
+ * Date: 20.04.2017
+ * Time: 22:33
+ */
+
+
+namespace CarBundle\Entity;
+
+//use FOS\UserBundle\Entity\User as BaseUser;
+use Doctrine\ORM\Mapping as ORM;
+use FOS\UserBundle\Model\User as BaseUser;
+use Symfony\Component\Security\Core\User\User;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="fos_user")
+ */
+class Ncvuser extends BaseUser
+{
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+
+    public function __construct()
+    {
+        parent::__construct();
+        // your own logic
+    }
+}
